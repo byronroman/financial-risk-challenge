3b2a27c0cfd4dee11d0e52ad9529ad3cd2ce45bb
byronroman <by.romana3@gmail.com>
2026-09-10 23:38:56 -0300

    chore: remove default nest files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ScoreModule } from './score/score.module';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, ScoreModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
