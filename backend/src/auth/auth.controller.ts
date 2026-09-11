04692b3fe06305d5ee908727694b074753485b6c
byronroman <by.romana3@gmail.com>
2026-09-10 22:44:53 -0300

    feat: implement jwt authentication

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Post, HttpCode, HttpStatus, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+
+@Controller()
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto.username, loginDto.password);
+  }
+}
--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,14 +1,31 @@
 import { Module } from '@nestjs/common';
 import { MockUserRepository } from './repositories/mock-user.repository';
 import { USER_REPOSITORY } from './repositories/user.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.getOrThrow<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: Number(configService.getOrThrow<string>('JWT_EXPIRES_IN')),
+        },
+      }),
+    }),
+  ],
+  controllers: [AuthController],
   providers: [
+    AuthService,
     {
       provide: USER_REPOSITORY,
       useClass: MockUserRepository,
     },
   ],
-  exports: [USER_REPOSITORY],
+  exports: [USER_REPOSITORY, JwtModule],
 })
 export class AuthModule {}
